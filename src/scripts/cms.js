447e1de4cf6d6b8deb2e161d16a539db48891862
hussein11a <husseinalnahar11@gmail.com>
2025-05-28 21:10:50 +0300

    Create cms.js

---
+++ src/scripts/cms.js
@@ -0,0 +1,10 @@
+// تخصيصات لوحة التحكم Netlify CMS
+if (window.netlifyIdentity) {
+  window.netlifyIdentity.on("init", (user) => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
+}
