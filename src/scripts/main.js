2d9e21fc58893a0910e2a57ebbc2aeb29fa8b613
hussein11a <husseinalnahar11@gmail.com>
2025-05-29 16:32:22 +0300

    Update main.js

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,7 +1,10 @@
-cms.js
-loadMetadata.js
-loadServices.js
-loadSettings.js
-loadTheme.js
-main.js
-ui.js
+// src/scripts/main.js
+
+import './loadSettings.js';
+import './loadServices.js';
+import './loadMetadata.js';
+import './loadTheme.js';
+import './ui.js';
+import './cms.js';
+import './loadContact.js';
+import './loadPages.js';  
